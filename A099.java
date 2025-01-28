84f4fed834a89eb9fc30d69c5a2da266a7c1a245
LukeSeobyungju <22100356@handong.ac.kr>
2025-01-28 15:29:00 +0900

    100

--- A099.java
+++ A099.java
@@ -1,2 +1,17 @@
+import java.util.Stack;
+
 public class A099 {
+    public boolean isValid(String s) {
+        Stack stack=new Stack();
+        for(int i=0;i<s.length();i++){
+            if(!stack.empty() && check(stack.peek().toString().charAt(0),s.charAt(i))) stack.pop();
+            else stack.push(s.charAt(i));
+        }
+        if(stack.empty()) return true;
+        else return false;
+    }
+    boolean check(char a, char b){
+        if(a=='(' && b==')' || a=='{' && b=='}' || a=='[' && b==']') return true;
+        else return false;
+    }
 }
--- A100.java
+++ A100.java
@@ -1,2 +1,10 @@
+import DS.TreeNode;
+
 public class A100 {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null) return 0;
+        if(root.val>high) return rangeSumBST(root.left,low,high);
+        else if(root.val<low) return rangeSumBST(root.right,low,high);
+        else return root.val + rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high);
+    }
 }
